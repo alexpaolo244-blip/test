03e4c3d528a8ea390c265d7a880ee2dd940c628e
alexpaolo244-blip <alexpaolo244@gmail.com>
2026-02-11 20:01:32 +0100

    Update and rename MainActivity.kt to SplashActivity.kt

---
+++ SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.shofyou.android
+
+import android.content.Intent
+import android.os.*
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1500)
+    }
+}
